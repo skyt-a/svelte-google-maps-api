068e6f091d2e84ceef4973774367c3e3b02ecb39
RinGoku <skyta80105@gmail.com>
2025-05-05 22:22:41 +0900

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
-	kit: {
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/svelte-google-maps-api' : ''
-		}
-	}
+	preprocess: vitePreprocess()
 };
 
 export default config;
